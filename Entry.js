f47c10babdae2f22f25db58d9186ee027c0ab14b
azmi72 <azmiali7221@gmail.com>
2023-01-16 22:20:33 +0700

    Latihan ExpressJs

---
+++ Entry.js
@@ -0,0 +1,58 @@
+import express from 'express';
+import cors from 'cors';
+import fs from 'fs';
+
+// Syntax Import untuk Version NodeJs ke bawah
+// const express = require('express');
+// const cors = require('cors');
+// const fs = require('fs');
+
+const entry = express();
+const port = 3001;
+
+entry.use(cors())
+
+entry.get('/', (req, res) => {
+    res.send(' Connected...')
+})
+
+entry.get('/banner', (req, res) => {
+    const data = getData('./Data/Banner.json');
+    res.json(data)
+})
+
+entry.get('/category', (req, res) => {
+    const data = getData('./Data/Category.json');
+    res.json(data)
+})
+
+entry.get('/product', (req, res) => {
+    const data = getData('./Data/Product.json');
+    res.json(data)
+})
+
+entry.get('/product/:id', (req, res) => {
+    const data = findData(req.params.id)
+    res.json(data)
+})
+
+entry.listen(port, () => {
+    console.log(`Server sudah berjalan di localhost port: ${port}`)
+})
+
+const getData = (path) => {
+    const data = fs.readFileSync(path, 'utf-8', (err, data) => data)
+    return JSON.parse(data)
+}
+
+const findData = (id) => {
+    const dataProduct = getData('./Data/Product.json')
+    const findProduct = dataProduct.find((data) => data.id === parseInt(id))
+    if(!findProduct) {
+        let dummy = [{
+            'brand': 'PRODUK TIDAK DITEMUKAN'
+        }]
+        return dummy
+    }
+    return findProduct
+}
